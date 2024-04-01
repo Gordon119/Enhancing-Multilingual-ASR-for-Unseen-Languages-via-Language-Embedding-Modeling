7936d31734c58d2212b7a0a32eeeeae25de6ba22
Gordon119 <gordonsmart119@gmail.com>
2024-04-01 11:13:04 +0000

    update

--- taiwanese2chinese.py
+++ taiwanese2chinese.py
@@ -3,13 +3,21 @@ import openai
 import argparse
 import pandas as pd
 
-openai.api_key=""
+openai.api_key="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"
 
 def translate(client, text):
     response = client.chat.completions.create(
-        model="gpt-4-turbo",
+        model="gpt-4-turbo-preview",
         messages=[
-            {"role": "system", "content": "請將下列台文文章轉換成繁體中文"},
+            {"role": "system", "content": 
+             "你是一位台語老師，要將下列台文的句子轉換成繁體中文，如同以下兩個範例\n\
+             1.\n \
+             今仔日油價一公升起五角。\n \
+             今天油價每公升上漲五角。\n \
+             2.\n \
+             淡水明仔載的溫度二十八到三十度，落雨機率四十％。\n \
+             淡水今天的溫度二十八到三十度，降雨機率四十％。\n \
+             ，請不要更改原本的語意或加入其他不相關的內容，並請直接回答答案。"},
             {"role": "user", "content": text}
         ]
     )
@@ -21,10 +29,12 @@ def main():
     parser.add_argument("-o", "--output", type=str)
     args = parser.parse_args()
     
-    client = openai.OpenAI()
+    client = openai.OpenAI(api_key=openai.api_key)
     df = pd.read_csv(args.file)
-    for i in range(len(df)):
-        df.iloc[i]["text"] = translate(client, df.iloc[i]["text"])
+    from tqdm import tqdm
+    for i in tqdm(range(len(df))):
+        df.loc[i, "text"] = translate(client, df.loc[i, "text"])
+        print(df.loc[i, "text"])
     df.to_csv(args.output, index=None)
 
 if __name__ == "__main__":
